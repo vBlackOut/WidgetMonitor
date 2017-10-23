ef4ce7178fb55f662cbe37cebf1c0dc2ffdec09d
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2017-10-23 17:06:43 +0200

    # add fan & optimize the script

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import time
 import requests
 import psutil
 import re
+import sys
 
  
 class Fader(wx.Frame):
  
     def __init__(self):
+        global label_fan
         no_sys_menu = wx.CAPTION
-        wx.Frame.__init__(self, None, title='Test', size=(200, 150), style=no_sys_menu)
+        wx.Frame.__init__(self, None, title='WidgetMonitor', size=(200, 220), style=no_sys_menu)
         self.amount = 200
         self.delta = 5
-        panel = wx.Panel(self, wx.ID_ANY)
+        #self.ToggleWindowStyle(wx.STAY_ON_TOP)
+        style = wx.TRANSPARENT_WINDOW if sys.platform.lower() == 'win32' else 0
+        panel = wx.Panel(self, wx.ID_ANY, style=style)
+        if panel.CanSetTransparent:
+            panel.SetTransparent(100)
  
         self.SetTransparent(self.amount)
         self.Maximize(False)
@@ -29,12 +35,27 @@ class Fader(wx.Frame):
         self.labelIpEXT = wx.StaticText(panel, wx.ID_ANY, label="Your ip Externe : 0.0.0.0", pos=(0,75), size=(200,20), style=wx.ALIGN_CENTRE)
         self.labeltemp0 = wx.StaticText(panel, wx.ID_ANY, label="temp0 : 0°C", pos=(0,100), size=(200,20), style=wx.ALIGN_CENTRE)
 
+        correct = 0
+        label_fan = {}
+        data_fans = psutil.sensors_fans()
+        for i in range(0, 9):
+            data2 = re.sub('sfan', '', str(data_fans['w83793'][i]))
+            data2 = re.sub('[()]', '', str(data2))
+            data2 = data2.split(",")
+            data2[1] = data2[1].replace("current=", "")
+            if int(data2[1]) > 0:
+                correct = correct + 1
+                print('label_fan' + str(i))
+                label_fan[str(i)] = wx.StaticText(panel, wx.ID_ANY, label="fan "+ str(i) +" : "+data2[1]+" RPM", pos=(0,100+correct*20), size=(200,20), style=wx.ALIGN_CENTRE)
+
         #self.timer = wx.Timer(self, wx.ID_ANY)
         #self.timer.Start(60)
         #self.Bind(wx.EVT_TIMER, self.AlphaCycle)
         self.Bind(wx.EVT_ENTER_WINDOW, self.onMouseOver)
         self.Bind(wx.EVT_LEAVE_WINDOW, self.onMouseLeave)
         ## ---------------------------- ##
+
+
 
     def onToggle(self, event):
         btnLabel = self.toggleBtn.GetLabel()
@@ -53,6 +74,15 @@ class Fader(wx.Frame):
 
         # get temperature
         data = psutil.sensors_temperatures()
+        data_fans = psutil.sensors_fans()
+        for i in range(0, 9):
+            data2 = re.sub('sfan', '', str(data_fans['w83793'][i]))
+            data2 = re.sub('[()]', '', str(data2))
+            data2 = data2.split(",")
+            data2[1] = data2[1].replace("current=", "")
+            if int(data2[1]) > 0:
+                label_fan[str(i)].SetLabel("fan "+ str(i) +" : "+data2[1]+" RPM")
+
         data2 = re.sub('shwtemp', '', str(data['w83793'][0]))
         data2 = re.sub('[()]', '', str(data2))
         data2 = data2.split(",")
@@ -62,6 +92,32 @@ class Fader(wx.Frame):
         self.labelIpEXT.SetLabel("Your ip Externe : "+ipEXT.text)
         self.labeltemp0.SetLabel("Temp0 : "+data2[1]+"°C")
 
+        if float(data2[1]) >= 0 or int(data2[1]):
+            try:
+                if int(data2[1]) < 55:
+                    self.labeltemp0.SetForegroundColour((127,255,0)) # set text color
+                if int(data2[1]) > 55:
+                    self.labeltemp0.SetForegroundColour((255,150,0)) # set text color
+                if int(data2[1]) > 80:
+                    self.labeltemp0.SetForegroundColour((255,0,0)) # set text color
+                if int(data2[1]) > 85:
+                    self.labeltemp0.SetLabel("Temp0 : "+data2[1]+"°C Warning /!\\")
+                    self.labeltemp0.SetForegroundColour((255,0,0)) # set text color
+            except ValueError:
+                if float(data2[1]) < 55:
+                    self.labeltemp0.SetForegroundColour((127,255,0)) # set text color
+                if float(data2[1]) > 55:
+                    self.labeltemp0.SetForegroundColour((255,150,0)) # set text color
+                if float(data2[1]) > 80:
+                    self.labeltemp0.SetForegroundColour((255,0,0)) # set text color
+                if float(data2[1]) > 85:
+                    self.labeltemp0.SetLabel("Temp0 : "+data2[1]+"°C Warning /!\\")
+                    self.labeltemp0.SetForegroundColour((255,0,0)) # set text color
+        else:
+            self.labeltemp0.SetLabel("Temp0 : Error sensors !")
+            self.labeltemp0.SetForegroundColour((255,0,0)) # set text color
+
+
     def onMouseOver(self, event):
         # mouseover changes colour of button
         self.SetTransparent(255)
@@ -69,7 +125,7 @@ class Fader(wx.Frame):
 
     def onMouseLeave(self, event):
         # mouse not over button, back to original colour
-        self.SetTransparent(150)
+        self.SetTransparent(200)
         event.Skip()
  
     def AlphaCycle(self, evt):
